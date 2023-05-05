9e43182fb1ef8ba9085e5275afb448cb4be3af5f
kevyn pineda <pinedamontenegro1234@gmail.com>
2023-05-05 08:44:39 -0500

    agrego un spinner

---
+++ src/components/UI/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Spinner = () => {
+    return(
+        <>
+        <div className="loading">
+            <div className="spinner"></div>
+                <p>Cargando...</p>
+        </div>
+        </>
+    )
+}
